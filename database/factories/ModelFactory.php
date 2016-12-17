52bd5419c8c794aaec3add46c5f305cf05fe6231
Miguel Piedrafita <github@miguelpiedrafita.com>
2016-12-17 08:58:55 +0000

    Apply fixes from StyleCI

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,8 +16,8 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
+        'name'           => $faker->name,
+        'email'          => $faker->unique()->safeEmail,
         'remember_token' => str_random(10),
     ];
 });
