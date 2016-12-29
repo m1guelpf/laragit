38e95eb6c0b8af49fd7a5751134144a04df43d88
Miguel Piedrafita <github@miguelpiedrafita.com>
2016-12-29 19:55:34 +0100

    Remove sendWelcome() from MailController

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -12,20 +12,6 @@ class MailController extends Controller
         $this->middleware('auth');
     }
 
-    public function sendWelcome()
-    {
-        if (Auth::user()['recieveMails']) {
-            $beautymail = app()->make(Beautymail::class);
-            $beautymail->send('emails.welcome', ['user' => Auth::user()], function ($message) {
-                $user = Auth::user();
-                $message
-            ->from('laragit@miguelpiedrafita.com')
-            ->to($user->email, $user->name)
-            ->subject('Welcome!');
-            });
-        }
-    }
-
     public function showUnsubscribe()
     {
         return redirect('wip');
