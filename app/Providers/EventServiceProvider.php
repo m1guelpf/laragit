3782a04dd6a6667d041f5c305e015c3a7d8f9241
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-01-04 13:08:14 +0100

    Update EventServiceProvider.php

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,8 +13,8 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\UpdateNotification' => [
-            'App\Listeners\NotificationsUpdated',
+        'App\Events\SomeEvent' => [
+            'App\Listeners\EventListener',
         ],
     ];
 
